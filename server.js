7fc84f2abada4a7a8ccabd9e1a7f25d6c03df653
Boris <borya.ivchenko@mail.ru>
2017-02-21 15:23:07 +0000

    completed

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var app = express();
-var http = require('http');
+var multer  = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+ 
+var upload = multer({ storage: storage })
+var uploadFile = upload.single('file');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 
-var server = http.createServer(app).listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
+app.post('/filesize', function (req, res, next) {
+    uploadFile(req, res, function(err) {
+        if (err) throw err;
+        var result = {fileSize: req.file.size};
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(result));
+        fs.unlinkSync("./uploads/" + req.file.filename);
+    })
+})
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
   console.log("App now running on port", port);
 });
